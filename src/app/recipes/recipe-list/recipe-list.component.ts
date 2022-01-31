46d69fe341a262a1acb9d9b711514c4ebe5b39f7
sravan reddy <sravanreddy234@gmail.com>
2022-01-31 15:59:18 +0530

    Outputting the Recipe List using ngFor
    Outputting the Recipe List using ngFor

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,8 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes: Recipe[] = [new Recipe('Burger','Non veg Meal','D:\Project_images\Burger.png')];
+  //recipes: Recipe[] = [new Recipe('Burger','Non veg Meal','D://ProjectImages//Burger.png')];
+  recipes: Recipe[] = [new Recipe('Burger','Non veg Meal','https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngitem.com%2Fmiddle%2FTRbhiiR_food-items-images-png-transparent-png%2F&psig=AOvVaw0nrcd1folpu08AEfFh8KFg&ust=1643702791871000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCLiftprE2_UCFQAAAAAdAAAAABAD')];
   constructor() { }
 
   ngOnInit(): void {
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: Ingredient[] = [new Ingredient("Burger", 199),
+  new Ingredient("Pizza", 149)];
   constructor() { }
 
   ngOnInit(): void {
