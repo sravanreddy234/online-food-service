6fb03aa36ab99090d7acf1926e609e1772ade695
sravan reddy <sravanreddy234@gmail.com>
2022-01-31 12:39:37 +0530

    Added Recipe list
    Added Recipe list along with Recipe Image

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes: Recipe[] = [new Recipe('Burger','Non veg Meal','D:\Project_images\Burger.png')];
   constructor() { }
 
   ngOnInit(): void {
